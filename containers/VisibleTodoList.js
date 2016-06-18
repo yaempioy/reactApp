1231951e153b58ddcbaac34e728b62d8e5323288
kitti16 <kitti.b@ensogo.com>
2016-06-18 16:44:45 +0700

    Container Components: How things work (data fetching, state update).

---
+++ containers/VisibleTodoList.js
@@ -0,0 +1,38 @@
+import { connect } from 'react-redux'
+import { VisibilityFilters } from '../actions/types'
+import TodoList from '../components/TodoList'
+import { toggleTodo } from '../actions/creators'
+
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed)
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
+
+export default VisibleTodoList
